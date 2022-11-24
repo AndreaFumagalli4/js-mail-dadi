e1d7d818c48ec46ddaa307c7e33751fea7cc11a3
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-24 16:08:32 +0100

    add email array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// 
 /** GIOCO DEI DADI:
  *  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
     Stabilire il vincitore, in base a chi fa il punteggio più alto.
@@ -38,3 +37,36 @@ if ( playerNumber > computerNumber ) {
 
 console.log(winner);
 document.getElementById('output-3').innerHTML = winner;
+
+
+
+/** EMAIL CHECK
+ * Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio   appropriato sull’esito del controllo.
+ * 
+ * 1. Chiedere all'utente la sua email;
+ * 2. Controlla se l'email è presente nella lista:
+ *      2.1 Creare una lista di tot email (ARRAY);
+ *      2.2 SE l'email dell'utente è nella lista ==> Stampare mex 'puoi accedere';
+ *      2.3 SE l'email non è nella lista ==> stampare messaggio 'Non puoi accedere; per accedere inserire un'email valida'.
+ */
+
+// 1.
+
+const userEmail = document.getElementById('mail');
+
+console.log(userEmail.value);
+
+// 2.1
+
+const emailList = [
+    'iron.man@gmail.com',
+    'hulk@gmail.com',
+    'captain.america@gmail.com',
+    'winter.soldier@gmail.com',
+    'falcon.eye@gmail.com',
+    'hammer.thor@gmail.com',
+    'black.widow@gmail.com',
+    'black.panther@gmail.com',
+    'guardians.galaxy@gmail.com',
+    'vision.ultron@gmail.com',
+]
